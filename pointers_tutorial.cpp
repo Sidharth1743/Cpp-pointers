f5c4bb3217f5878d5d783c68c6eed80c24279ca8
Sidharth1743 <sid250581@gmail.com>
2026-07-05 21:29:17 +0530

    pointers till typecasting

---
+++ pointers_tutorial.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // Working with pointers;
+    int a;
+    int *p;
+    a = 10;
+    p = &a;
+    cout << "Value of a " << a << "\n";
+    cout << "Address of p is " << p << "\n";
+    *p = 23;
+    cout << "Valude of a is " << a << "\n";
+    cout << "Address of a is " << p << "\n"; // Address not changed
+
+    // pointer arithmetic
+    cout << "Address of a " << p << "\n";
+    cout << "Size of int " << sizeof(int) << " Bytes\n";
+    cout << "Address of a+1 " << p + 1 << "\n"; // increased by 4
+
+    // why datatype is important for pointers
+    int b = 1025;
+    int *j;
+    j = &b;
+    cout << "Address of b is " << j << "\n";
+    cout << "Value of b is " << *j << "\n";
+    char *p0;
+    p0 = (char *)j; // typecasting
+    cout << "Size of char " << sizeof(char) << " Bytes\n";
+    cout << "Address of p0 " << &p0 << "\n";
+    cout << "Address-void of p0 " << (void *)p0 << "\n";
+    cout << "Value of p0 " << *p0 << "\n";
+}
