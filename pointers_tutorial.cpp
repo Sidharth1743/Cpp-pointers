213f9348f2bfc587e42f0fdad644871f9ec00220
Sidharth1743 <sid250581@gmail.com>
2026-07-05 21:49:08 +0530

    typecasting: generic pointers

--- pointers_tutorial.cpp
+++ pointers_tutorial.cpp
@@ -24,12 +24,16 @@ int main()
     int b = 1025;
     int *j;
     j = &b;
-    cout << "Address of b is " << j << "\n";
-    cout << "Value of b is " << *j << "\n";
+    cout << "Address of j is " << j << "\n"; // same as address of b
+    cout << "Value of j is " << *j << "\n";
     char *p0;
     p0 = (char *)j; // typecasting
     cout << "Size of char " << sizeof(char) << " Bytes\n";
-    cout << "Address of p0 " << &p0 << "\n";             // very important : p0 as another address in which typecasting occurs there
+    cout << "Address of p0 " << &p0 << "\n";             // very important : p0 is another address in which typecasting occurs there
     cout << "Address-void of p0 " << (void *)p0 << "\n"; // prints address of b
-    cout << "Value of p0 " << *p0 << "\n";
+    cout << "Value of p0 " << *p0 << "\n";               // 1 won't print repesents as ASCII
+    // void pointers - generic pointer
+    void *p1;
+    p1 = j;
+    cout << "address of p1 is " << p1 + 1 << "\n"; // gcc on linux defauls to 1 , so + 1 is added or else fails in microsoft compiler
 }
