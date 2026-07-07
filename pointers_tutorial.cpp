d606175519ef7658d6577af56fd8b8056c5547b2
Sidharth1743 <sid250581@gmail.com>
2026-07-07 13:10:32 +0530

    completed

--- pointers_tutorial.cpp
+++ pointers_tutorial.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+void Increment(int *p)
+{
+    *p = *p + 1;
+}
+
+void print(char *D)
+{
+    int i = 0;
+    while (D[i] != '\0')
+    {
+        cout << *D;
+        D++;
+    }
+    cout << "\n";
+}
+
+int ADD(int x, int y)
+{
+    int c = x + y;
+    cout << "Address of x in function ADD" << &x << "\n"; // address is different
+    return c;
+}
+
 int main()
 {
     // Working with pointers;
@@ -36,4 +59,52 @@ int main()
     void *p1;
     p1 = j;
     cout << "address of p1 is " << p1 + 1 << "\n"; // gcc on linux defauls to 1 , so + 1 is added or else fails in microsoft compiler
+
+    // pointers to pointers
+    int d = 15;
+    int *e = &d;
+    *e = 13;
+    int **f = &e;
+    int ***g = &f;
+    cout << "e " << e << "\n";
+    cout << "d " << &d << "\n";
+    cout << "*e " << *e << "\n";
+    cout << "*f " << *f << "\n";
+    cout << "f " << f << "\n";
+    cout << "**f " << **f << "\n";
+    cout << "**g " << **g << "\n";
+    cout << "***g " << ***g << "\n";
+    ***g = 130;
+    cout << "d " << d << "\n";
+
+    // Call by refernce - pointers function
+    Increment(&d);
+    cout << "Value of d after increment " << d << "\n"; // saves memory eventhough copies extra
+
+    char C[4];
+    C[0] = 'S';
+    C[1] = 'A';
+    C[2] = 'C';
+    C[3] = 'h';
+    cout << C << "\n";
+
+    // character arrays and pointers
+    char D[10] = "Sachiv";
+    print(D);
+
+    // pointers and multi dimensional arrays
+    int J[3][2][2] = {
+        {{2, 5}, {3, 7}},
+        {{3, 4}, {5, 6}},
+        {{7, 9}, {8, 2}}};
+    cout << J << "\n"
+         << *J << "\n"
+         << J[0] << "\n"
+         << &J[0][0] << "\n";
+    cout << *(J[0][0] + 1) << "\n";
+
+    int x = 4, y = 3;
+    int z = ADD(x, y);
+    cout << "address of x in function main " << &x << "\n"; // address is different
+    cout << "Addition of a and b " << z;
 }
